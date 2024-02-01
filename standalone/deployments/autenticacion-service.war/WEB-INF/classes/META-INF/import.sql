a9fb98c1bff31486006c9c071f71ab26d6146970
KaarJoseph <kaarunkuch@gmail.com>
2024-01-31 19:59:05 -0500

    first commit

--- standalone/deployments/autenticacion-service.war/WEB-INF/classes/META-INF/import.sql
+++
@@ -1,4 +0,0 @@
--- Insertar datos de ejemplo en la tabla Usuario
-INSERT INTO usercredentials (id, username, password) VALUES (1, 'usuario1', 'clave1');
-INSERT INTO usercredentials (id, username, password) VALUES (2, 'usuario2', 'clave2');
--- Otros inserts...
